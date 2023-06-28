15569e882ebd8e384dae257b2d13ce9e680c95aa
DevanapalliLahari <137879391+DevanapalliLahari@users.noreply.github.com>
2023-06-28 21:31:10 +0530

    Add files via upload

---
+++ Task-June-7/Mobile.java
@@ -0,0 +1,23 @@
+class Mobile{
+	Mobile()
+	{
+		System.out.println("Mobile is running");
+	}
+}
+class MobileRunner{
+	public static void main(String[]args)
+	{
+		System.out.println("The main is running");
+		Mobile mobile=new Mobile();
+		Mobile number=new Mobile();
+		Mobile type=new Mobile();
+		Mobile internet=new Mobile();
+		Mobile contacts=new Mobile();
+		Mobile applications=new Mobile();
+		Mobile photos=new Mobile();
+		Mobile videos=new Mobile();
+		Mobile mail=new Mobile();
+		Mobile account=new Mobile();
+		Mobile info=new Mobile();
+	}
+}
