e25e7792e848db85adfe74ac269b664aa2c61828
DevanapalliLahari <dlaharixworkz@gmail.com>
2023-06-30 13:52:11 +0530

    test

---
+++ test/javatest.java
@@ -0,0 +1,64 @@
+1.Java:-java is a programming language,platform independent because once the byte ccode generated it can execute at any plantform.. 
+javac:-javac stands for java compiler.here the compiler generaate the byte code.the byte code will store in .class.
+
+2.JDK:-It stands for Java Development Kit.here we have two files one is javac and another one is java.The JDK compile the program and generate byte code.
+JRE:-It stands for Java Runtime Execution.here we have one file that is java.It is execute the program which is in byte code.
+JVM:-The JVM stands for Java Virtual Machine.it is also do the execution part but Virtually,we can't seee the process.
+
+3.Limitations of Array:-
+it consist of length whieh is used to know about the index of the this process is  known as Array retrive.
+the Array means Container which store all the real time object.
+in this array we can address of the variable and also length of the dataa type.
+
+4.Class:-class is a blue Print of the class.it also a key word.The class declaration shoud be in capital.
+Object:-It is instance of a claass
+
+5.Array:-the array is a container which stores all the real time object.
+
+6..javafile:-the .javafile contain the program which  we write that means in english.
+.classfile:- the .classfile contains byte code which is generated after the compilation.
+
+7.the length is=5.
+
+8.the method signature is
+    static viod data type();
+	
+9.Java:-java is a programming language and platform independent.
+
+10Variable:-the variable stores the reference of the initilization.
+	class vriable
+	static variable
+	non static variable
+	
+11.class variable:-the class variable is the declaration of class that meaans class name which is given to the class.
+
+12.data type:-data type is has two types they are primitive and non primitive
+primitive has 8 and the nonprimitive data data types are string and any other declare by developer.
+
+13.Features:-
+platform independent.
+easy to understand.
+it can write in english
+
+14Types of arrays:-int,String,list,double,float
+
+15.the index out of the box means the length of the array is wrong that is the has the if we give the wrong index then we can observe this error.
+
+16.System.out.println:-here System means pre defined class 
+                      "." means the operater which is used to concatinate.
+					  out means static variable.
+					  println means static method.
+					  
+17.Double equal operator:-the double equal operator used for to know the boolean.that means whether the condition is true or false.
+
+18.dot operator:-It is used for to concatinate the variables in the class.
+
+19. the base class means the class which is written in the first with main method.
+
+20.byte,int,short,boolean,long,double,float,private,public,String,class,else,import
+
+					  
+	
+	
+	
+	
